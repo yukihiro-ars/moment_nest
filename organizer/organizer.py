3832626b09dc40ee67f99695d42dbfab3ec36cbb
yukihiroadachi.biz <yukihiroadachi.biz@gmail.com>
2024-02-10 15:38:57 +0900

    拡張子の大文字/小文字をまとめて管理　コメント見直し

--- organizer/organizer.py
+++ organizer/organizer.py
@@ -3,7 +3,7 @@ import os
 import questionary
 import glob
 
-TARGET_EXT = ['mp4', 'jpeg', 'jpg', 'png', 'gif']
+TARGET_EXT = ['mp4', 'mov', 'jpeg', 'jpg', 'png', 'gif']
 MSG_SELECT_PATH_1 = '整理対象のフォルダを選択してください'
 MSG_SELECT_PATH_2 = 'フォルダ指定が不正です\nフォルダを再選択してください'
 
@@ -25,25 +25,27 @@ def do_organize():
     dict_ext = {}
     # 拡張子ごとに分類
     for file in files:
-        # 拡張子取得
-        ext = file.rsplit('.', 1)[1]
+        # 拡張子取得(小文字統一)
+        ext = file.rsplit('.', 1)[1].lower()
         # 拡張子ごとに分類
         if ext not in dict_ext:
             dict_ext[ext] = []
         dict_ext[ext].append(file)
 
+    # ファイル全量の把握
     for ext in dict_ext.keys():
+        length = len(dict_ext[ext])
         if ext in TARGET_EXT:
-            print(f'target ext = {ext}')
+            print(f'target ext = {ext}, len = {length}')
+            # Backupフォルダに退避してから処理開始(ファイルごとに実施する想定)
+            # 画像の場合、EXIF、動画の場合は日付情報を取得して新規ファイル名の作成
+            # フォルダ存在チェック＆フォルダ作成
+            # 画像コピー＆リネーム
+            # 完了後Backupフォルダの対象ファイルを削除(ファイルごとに実施する想定)
         else:
-            print(f'not target ext = {ext}')
+            print(f'no target ext = {ext}, len = {length}')
+            # 対象外ファイルの退避処理
+
         for file in dict_ext[ext]:
             print(f'\t{file}')
 
-    # ■処理内容検討
-    # ファイル全量の把握
-    # Backupフォルダに退避してから処理開始(ファイルごとに実施する想定)
-    # 画像の場合、EXIF、動画の場合は日付情報を取得して新規ファイル名の作成
-    # フォルダ存在チェック＆フォルダ作成
-    # 画像コピー＆リネーム
-    # 完了後Backupフォルダの対象ファイルを削除(ファイルごとに実施する想定)
